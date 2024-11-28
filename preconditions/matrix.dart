515fc19673de84756ddcd8094500856d508e636a
yankozyrenko <kopn56@gmail.com>
2024-11-28 13:09:31 +0300

    precondition examples

---
+++ preconditions/matrix.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: unused_element
+
+import '../annotation.dart';
+
+@Contract()
+class Matrix {
+  final List<List<double>> data;
+
+  @Precondition({
+    'data.isNotEmpty && data.first.isNotEmpty':
+        'Matrix dimensions must be greater than zero.',
+  })
+  Matrix._(this.data);
+
+  @Precondition({
+    'other.data.length == data.length && other.data.first.length == data.first.length':
+        'Matrices must have the same dimensions for addition.',
+  })
+  Matrix _add(Matrix other) {
+    List<List<double>> result = [];
+    for (int i = 0; i < data.length; i++) {
+      List<double> row = [];
+      for (int j = 0; j < data[i].length; j++) {
+        row.add(data[i][j] + other.data[i][j]);
+      }
+      result.add(row);
+    }
+    return Matrix._(result);
+  }
+
+  @Precondition({
+    'data.first.length == other.data.length':
+        'Number of columns of the first matrix must equal the number of rows of the second matrix for multiplication.',
+  })
+  Matrix _multiply(Matrix other) {
+    List<List<double>> result = [];
+    for (int i = 0; i < data.length; i++) {
+      List<double> row = [];
+      for (int j = 0; j < other.data[0].length; j++) {
+        double sum = 0;
+        for (int k = 0; k < data[i].length; k++) {
+          sum += data[i][k] * other.data[k][j];
+        }
+        row.add(sum);
+      }
+      result.add(row);
+    }
+    return Matrix._(result);
+  }
+}
---
+++ preconditions/user_registration.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: unused_element
+
+import '../annotation.dart';
+
+@Contract()
+class UserRegistration {
+  @Precondition({
+    'username.isNotEmpty': 'Username must not be empty.',
+    'username.length <= 15': 'Username must be 15 characters or fewer.',
+    'email.contains("@")': 'Email must contain an "@" symbol.',
+    'password.length >= 8': 'Password must be at least 8 characters long.',
+  })
+  void _registerUser(String username, String email, String password) {
+    // Registration logic
+  }
+}
