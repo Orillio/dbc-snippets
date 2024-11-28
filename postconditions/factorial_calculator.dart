2a051caa13fee6020397e7f318d534d7ad51617c
yankozyrenko <kopn56@gmail.com>
2024-11-28 15:32:55 +0300

    postcondition examples

---
+++ postconditions/factorial_calculator.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: unused_element
+
+import '../annotation.dart';
+
+@Contract()
+class FactorialCalculator {
+  @Postcondition({
+    'result > 0': 'Factorial result must be greater than zero.',
+    'result == _expectedFactorial(n)': 'Result must match the expected factorial value.',
+  })
+  int _calculateFactorial(int n) {
+    if (n == 0 || n == 1) return 1;
+    return n * _calculateFactorial(n - 1);
+  }
+
+  int _expectedFactorial(int n) {
+    // A method that calculates the expected factorial value iteratively.
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+}
---
+++ postconditions/list_sorter.dart
@@ -0,0 +1,22 @@
+import '../annotation.dart';
+
+@Contract()
+class ListSorter {
+  @Postcondition({
+    '_isSorted(result)': 'The returned list must be sorted in ascending order.',
+  })
+  List<int> _sortList(List<int> unsortedList) {
+    List<int> sortedList = List.from(unsortedList);
+    sortedList.sort();
+    return sortedList;
+  }
+
+  bool _isSorted(List<int> list) {
+    for (int i = 0; i < list.length - 1; i++) {
+      if (list[i] > list[i + 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
