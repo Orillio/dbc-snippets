f51c883143276d6b342a88a9138dd6aa45ed2ead
RoukayaZaki <roka.zaki2002@gmail.com>
2024-11-28 15:01:32 +0200

    add code snippet

---
+++ preconditions and postconditions/max_list.dart
@@ -0,0 +1,14 @@
+import '../annotation.dart';
+
+@Contract()
+class MathUtils {
+  @Precondition({
+    'list.isNotEmpty': 'Input list must not be empty.',
+  })
+  @Postcondition({
+    'list.contains(result)': 'The result must be an element of the list.',
+  })
+  int _findMax(List<int> list) {
+    return list.reduce((a, b) => a > b ? a : b);
+  }
+}
